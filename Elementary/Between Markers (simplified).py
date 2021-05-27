c12d1f5b57c13915a9351eb840862c8c547cb4bb
AlexVBodrov <alexvbodrov@mail.ru>
2021-05-28 02:06:51 +0300

    закончил остров Elementary на 100%

--- Elementary/Between Markers (simplified).py
+++ Elementary/Between Markers (simplified).py
@@ -12,3 +12,22 @@ between_markers('What is >apple<', '>', '<') == 'apple'
 Как это используется: Может использоваться для парсинга небольшой верстки.
 Предусловия: Не может быть более одного маркера одного типа.
 """
+def between_markers(text: str, begin: str, end: str) -> str:
+    """
+        returns substring between two given markers
+    """
+    out = text[text.index(begin)+1:text.index(end)]
+    #return text[text.find(begin) + 1: text.find(end)]
+    return out
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(between_markers('What is >apple<', '>', '<'))
+
+    # These "asserts" are used for self-checking and not for testing
+    assert between_markers('What is >apple<', '>', '<') == "apple"
+    assert between_markers('What is [apple]', '[', ']') == "apple"
+    assert between_markers('What is ><', '>', '<') == ""
+    assert between_markers('>apple<', '>', '<') == "apple"
+    print('Wow, you are doing pretty good. Time to check it!')
